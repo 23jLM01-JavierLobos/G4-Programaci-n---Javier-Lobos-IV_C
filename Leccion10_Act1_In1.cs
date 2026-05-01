7ff640b42ca83ce6795c813a7062545440c0caf9
23jLM01-JavierLobos <lobos.ale.15@gmail.com>
2026-05-01 13:00:24 -0600

    Add files via upload

---
+++ Leccion10_Act1_In1.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int TOTAL_SELLOS = 8;
+        Console.WriteLine("----BIENVENIDO A LA BARBERÍA DON CRUP----");
+
+        for (int i = 1; i <=TOTAL_SELLOS; i++)
+        {
+            Console.WriteLine("\nSello actual {0} de {1} ", i, TOTAL_SELLOS);
+            Console.WriteLine("Presione cualquier tecla para Registar esta visita");
+            Console.ReadKey();
+
+            Console.WriteLine(">>Sellos #{} registrado correctamente...");
+        }
+
+        Console.WriteLine("\n***********************************************");
+        Console.WriteLine("CUPONERA COMPLETA");
+        Console.WriteLine("Orozco, has ganado tu corte de cabello GRATIS!!!");
+        Console.WriteLine("\n***********************************************");
+        Console.WriteLine("\nPresione una tecla para cerrar");
+        Console.ReadKey();     
+    }
+}
---
+++ Leccion10_Act1_In1.psc
@@ -0,0 +1,16 @@
+Algoritmo Leccion10_Act1_In1
+	Definir i Como Entero
+	
+	Escribir "-----CUPONERA BARBERÍA DON CRUP-----"
+	
+	Para i=1 Hasta 8 Con Paso 1 Hacer
+		Escribir "Presione una tecla para marcar el sello, numero ", i
+		Esperar Tecla
+		Escribir "¡SELLO! ", i , " marcado..."
+		Escribir "------------------------------"
+	Fin Para
+	
+	Escribir "¡Felicidades! has completado los 8 sellos"
+	Escribir "CORTE GRATIS!!!"
+	
+FinAlgoritmo
