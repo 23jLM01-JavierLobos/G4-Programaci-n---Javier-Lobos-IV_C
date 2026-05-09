e3d0eed061cb0adfdae5b7e5d3f3182399982651
23jLM01-JavierLobos <lobos.ale.15@gmail.com>
2026-05-09 12:57:03 -0600

    Add files via upload

---
+++ Lecc11_Act2_In3 (C#).cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main()
+    {
+        int num1, num2, menor, mayor, i;
+
+        Console.Write("Digite el primer numero: ");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite el segundo numero: ");
+        num2 = int.Parse(Console.ReadLine());
+
+        if (num1 < num2)
+        {
+            menor = num1;
+            mayor = num2;
+        }
+        else
+        {
+            menor = num2;
+            mayor = num1;
+        }
+
+        i = menor;
+
+        do
+        {
+            if (i % 4 == 0)
+            {
+                Console.WriteLine(i);
+            }
+
+            i++;
+        }
+        while (i <= mayor);
+    }
+}
